0ef0b131d333ececfa859769489c5fb1bc38c345
Ivan Sapozhnik <ivansapozhnik@Ivans-MacBook-Pro.local>
2013-10-07 21:39:49 +0300

    tableview added to the main controller view.

--- NavigationMenu/HAViewController.m
+++ NavigationMenu/HAViewController.m
@@ -8,6 +8,10 @@
 
 #import "HAViewController.h"
 
+@interface HAViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@end
+
 @implementation HAViewController
 
 - (void)viewDidLoad
@@ -21,6 +25,31 @@
         menu.delegate = self;
         self.navigationItem.titleView = menu;
     }
+    UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStylePlain];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    [self.view addSubview:tableView];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellId = @"CellId";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    
+    if (nil == cell)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"Cell %d",indexPath.row];
+    
+    return cell;
 }
 
 - (void)didSelectItemAtIndex:(NSUInteger)index
